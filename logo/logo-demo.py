78ab79e4c6c4df54f392a3cfe01177e6854c0f7e
Stephen Cummins <sacummins@gmail.com>
2013-12-19 03:02:13 -0800

    Running logo/logo-demo.py

--- logo/logo-demo.py
+++ logo/logo-demo.py
@@ -3,6 +3,7 @@ from tealight.logo import (move, turn,
                            show_turtle, hide_turtle,
                            color, speed)
 
+from time import sleep
 
 print "This is logo mode!"
 
@@ -10,6 +11,7 @@ colors = ["red", "blue", "green"]
 
 for i in range(10,200,5):
   move(i)
+  sleep(0.1)
   turn(120)
   c = colors[(i / 5)%3]
   color(c)
